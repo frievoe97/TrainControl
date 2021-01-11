0fb7911d4aa29b189207d68a98f1984024827ef4
frie_voe <friedrich.k.voelkers@tu-berlin.de>
2021-01-11 21:31:09 +0100

    update index.php to test prepare_fahrplan_session.php

--- php/index.php
+++ php/index.php
@@ -2,6 +2,7 @@
 
 require 'vorbelegung.php';
 require 'functions.php';
+require 'prepare_fahrplan_session.php';
 
 $fahrzeugdaten = [
 	"gbt_id" => "601",
@@ -12,6 +13,9 @@ $fahrzeugdaten = [
 var_dump(getFahrzeugdaten($fahrzeugdaten, "id"));
 
 
-var_dump(getFahrzeugimInfraAbschnitt(1186))
+var_dump(getFahrzeugimInfraAbschnitt(1186));
+
+deleteSessionFahrplan();
+
 
 ?>
