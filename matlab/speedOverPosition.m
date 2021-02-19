0d3b5d4e0895dfcd1604c5d444031f7bdd87a509
frie_voe <friedrich.k.voelkers@tu-berlin.de>
2021-02-20 00:42:45 +0100

    Adding figures in matlab

--- matlab/speedOverPosition.m
+++ matlab/speedOverPosition.m
@@ -10,4 +10,20 @@ speedOverPosition_x = val(:,1);
 speedOverPosition_y = val(:,2);
 
 
-plot(speedOverPosition_x,speedOverPosition_y);
+p = plot(speedOverPosition_x,speedOverPosition_y);
+p.LineWidth = 2;
+fontSize = 18;
+title("Geschwindigkeit in Abhähngigkeit der Position", 'FontSize', fontSize);
+xlabel("Strecke [m]", 'FontSize', fontSize);
+ylabel("Geschwindigkeit [km/h]", 'FontSize', fontSize);
+x0=10;
+y0=10;
+width=1100;
+height=600;
+axis([-10 500 0 55])
+set(gcf,'position',[x0,y0,width,height]);
+set(gca, 'FontSize', 14);
+t = gca;
+exportgraphics(t,'SpeedOverPosition.jpg','Resolution',300);
+
+
--- matlab/speedOverTime.m
+++ matlab/speedOverTime.m
@@ -10,4 +10,19 @@ speedOverTime_x = val(:,1)-1612811700;
 speedOverTime_y = val(:,2);
 
 
-plot(speedOverTime_x,speedOverTime_y);
+p = plot(speedOverTime_x,speedOverTime_y);
+
+p.LineWidth = 2;
+fontSize = 18;
+title("Geschwindigkeit in Abhähngigkeit der Zeit", 'FontSize', fontSize);
+xlabel("Strecke [m]", 'FontSize', fontSize);
+ylabel("Zeit [s]", 'FontSize', fontSize);
+x0=10;
+y0=10;
+width=1100;
+height=600;
+axis([-10 60 0 55])
+set(gcf,'position',[x0,y0,width,height]);
+set(gca, 'FontSize', 14);
+t = gca;
+exportgraphics(t,'SpeedOverTime.jpg','Resolution',300);
