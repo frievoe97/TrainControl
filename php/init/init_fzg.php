4279ba8e7728d7c0a82e24c6afb7b54e864c0968
frie_voe <friedrich.k.voelkers@tu-berlin.de>
2021-03-27 12:13:02 +0100

    Adding footnote quotes to the paper

--- php/init/init_fzg.php
+++ php/init/init_fzg.php
@@ -86,3 +86,96 @@ $fzgTest = array(
 	"next_timetable_change_time" => ""
 );
 
+
+
+
+function getAllTrains () : array {
+
+	$allAdresses = getAllAdresses();
+	$DB = new DB_MySQL();
+	$allTrains = array();
+
+	foreach ($allAdresses as $adress) {
+
+		$train = get_object_vars($DB->select("SELECT `".DB_TABLE_FAHRZEUGE."`.`id`, 
+							`".DB_TABLE_FAHRZEUGE."`.`adresse`, 
+							`".DB_TABLE_FAHRZEUGE."`.`timestamp`, 
+							`".DB_TABLE_FAHRZEUGE."`.`speed`, 
+							`".DB_TABLE_FAHRZEUGE."`.`dir`, 
+							`".DB_TABLE_FAHRZEUGE."`.`zugtyp`, 
+							`".DB_TABLE_FAHRZEUGE."`.`zuglaenge`, 
+							`".DB_TABLE_FAHRZEUGE."`.`prev_speed`, 
+							`".DB_TABLE_FAHRZEUGE."`.`fzs`, 
+							`".DB_TABLE_FAHRZEUGE."`.`verzoegerung`, 
+							`".DB_TABLE_FAHRZEUGE."`.`zustand`        
+                            FROM `".DB_TABLE_FAHRZEUGE."`
+                            WHERE `".DB_TABLE_FAHRZEUGE."`.`adresse` = $adress
+                           ")[0]);
+
+		$trainTwo = $DB->select("SELECT `".DB_TABLE_FAHRZEUGE_BAUREIHEN."`.`bezeichnung`, 
+							`".DB_TABLE_FAHRZEUGE_BAUREIHEN."`.`vmax`, 
+							`".DB_TABLE_FAHRZEUGE_BAUREIHEN."`.`traktion`      
+                            FROM `".DB_TABLE_FAHRZEUGE_BAUREIHEN."`
+                            WHERE `".DB_TABLE_FAHRZEUGE_BAUREIHEN."`.`nummer` = $adress
+                           ");
+
+		if (sizeof($trainTwo) != 0) {
+			$trainTwo = get_object_vars($trainTwo[0]);
+		} else {
+			$trainTwo["bezeichnung"] = null;
+			$trainTwo["vmax"] = null;
+			$trainTwo["traktion"] = null;
+		}
+
+		$returnArray = array_merge($train, $trainTwo);
+
+		array_push($allTrains, $returnArray);
+	}
+
+	unset($DB);
+
+	return $allTrains;
+}
+
+function getAllAdresses () : array {
+
+	$returnAdresses = array();
+	$DB = new DB_MySQL();
+
+	$adresses = $DB->select("SELECT DISTINCT `".DB_TABLE_FAHRZEUGE."`.`adresse`
+                            FROM `".DB_TABLE_FAHRZEUGE."`
+                           ");
+
+	unset($DB);
+
+	foreach ($adresses as $adressIndex => $adressValue) {
+		array_push($returnAdresses, (int) $adressValue->adresse);
+	}
+
+	return $returnAdresses;
+}
+
+function getNextBetriebsstellen (int $id) : array {
+
+	$DB = new DB_MySQL();
+	$returnBetriebsstellen = array();
+
+	$betriebsstellen = $DB->select("SELECT `".DB_TABLE_FAHRPLAN_SESSIONFAHRPLAN."`.`betriebsstelle`
+                            FROM `".DB_TABLE_FAHRPLAN_SESSIONFAHRPLAN."`
+                            WHERE `".DB_TABLE_FAHRPLAN_SESSIONFAHRPLAN."`.`zug_id` = $id
+                            ORDER BY `".DB_TABLE_FAHRPLAN_SESSIONFAHRPLAN."`.`id` ASC
+                           ");
+
+	unset($DB);
+
+	foreach ($betriebsstellen as $betriebsstellenIndex => $betriebsstellenValue) {
+		array_push($returnBetriebsstellen, $betriebsstellenValue->betriebsstelle);
+	}
+
+	if (sizeof($betriebsstellen) == 0) {
+		debugMessage("Zu dieser Zug ID sind keine nächsten Betriebsstellen im Fahrplan vorhanden.");
+	}
+
+	return $returnBetriebsstellen;
+}
+
--- php/newFunctions.php
+++ php/newFunctions.php
@@ -3,6 +3,8 @@
 require 'vorbelegung.php';
 require 'funktionen_abschnitte.php';
 require 'init/init_abschnitte.php';
+require 'init/init_fzg.php';
+require 'fahrplan_functions.php';
 
 $cacheInfranachbarn = createCacheInfranachbarn();
 $cacheInfradaten = createCacheInfradaten();
@@ -10,16 +12,37 @@ $cacheSignaldaten = createCacheSignaldaten();
 
 // ERROR
 //var_dump(getNaechsteAbschnitte(153, 1));
-var_dump(getNaechsteAbschnitte(354, 1));
+//var_dump(getNaechsteAbschnitte(354, 1));
 
 //var_dump(getSignalbegriff(89));
 //var_dump(getSignalbegriff(91));
 
+// Zeit:
+$DB = new DB_MySQL();
+$databaseTime = (float) strtotime($DB->select("SELECT CURRENT_TIMESTAMP")[0]->CURRENT_TIMESTAMP);
+unset($DB);
+$simulationTime = (float) getUhrzeit();
+$timeDifference = $databaseTime - $simulationTime;
 
 
 // Step 1: Initilize all trains (verzoegerung, laenge etc.)
 
+$allTrains = getAllTrains();
+
+
+foreach ($allTrains as $trainIndex => $trainValue) {
+	$allTrains[$trainIndex]["nextBetriebsstellen"] = getNextBetriebsstellen($trainValue["id"]);
+}
+
+// v_max, Bezeichnung, Baureihe, evu
+
 // Steop 2: Next stop and the sections between the current position and the next stop
+foreach ($allTrains as $trainIndex => $trainValue) {
+	$allTrains[$trainIndex]["nextBetriebsstellen"] = getNextBetriebsstellen($trainValue["id"]);
+	//$allTrains[$trainIndex]["nextSections"] = getNaechsteAbschnitte($trainValue["id"], $trainValue["dir"]);
+}
+
+var_dump($allTrains);
 
 // BIG LOOP
 
