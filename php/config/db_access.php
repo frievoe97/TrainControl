676ce49ca498821de161b5c7646d6cc8385c0a0c
frievoe97 <44405087+frievoe97@users.noreply.github.com>
2022-09-15 18:27:02 +0200

    Update db_access.php

--- php/config/db_access.php
+++ php/config/db_access.php
@@ -3,5 +3,5 @@
 define('DB_STANDARD', 1);
 $MySQL_config[1]['host'] = '127.0.0.1';
 $MySQL_config[1]['benutzer'] = 'root';
-$MySQL_config[1]['passwort'] = '1928hans';
+$MySQL_config[1]['passwort'] = 'password';
 $MySQL_config[1]['dbname'] = 'ebuef';
